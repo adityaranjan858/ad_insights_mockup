45eef3b3993d68b580fa24cd91925d4772072f37
adityaranjan858 <adityaranjan858@gmail.com>
2023-12-05 00:27:35 +0530

    changes settings of basename and package json

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base : "/ad_insights_mockup/",
+  base : "/ad_insights_mockup",
   plugins: [react()],
 })
